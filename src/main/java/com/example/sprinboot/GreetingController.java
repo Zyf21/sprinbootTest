734942d8903e4f0e245e51da4fc0aa4671e1d9bd
brigadin <brigadin@mail.com>
2018-08-06 17:53:24 +0300

    autorization

--- src/main/java/com/example/sprinboot/GreetingController.java
+++ src/main/java/com/example/sprinboot/GreetingController.java
@@ -18,16 +18,14 @@ public class GreetingController {
     @Autowired
     private MessageRepo messageRepo;
 
-    @GetMapping("/greeting")
-    public String greeting(
-            @RequestParam(name="name", required=false, defaultValue="World") String name,
-            Map<String, Object> model
-    ) {
-        model.put("name", name);
-        return "greeting";
-    }
+    @GetMapping("/")
+    public String greeting(Map<String, Object> model)
 
-    @GetMapping
+     {
+        return "greeting";
+     }
+
+    @GetMapping ("/main")
     public String main(Map<String, Object> model) {
         Iterable<Message> messages = messageRepo.findAll();
 
@@ -36,7 +34,7 @@ public class GreetingController {
         return "main";
     }
 
-    @PostMapping
+    @PostMapping ("/main")
     public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
         Message message = new Message(text, tag);
 
